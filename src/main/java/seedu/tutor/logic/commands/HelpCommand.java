33a187f42ee8cbeed6021f0f3e68d13a333f993a
Keiran <keinn2210@gmail.com>
2026-03-18 01:12:32 +0800

    Update help message with subject and relate features

--- src/main/java/seedu/tutor/logic/commands/HelpCommand.java
+++ src/main/java/seedu/tutor/logic/commands/HelpCommand.java
@@ -13,16 +13,20 @@ public class HelpCommand extends Command {
             + "Example: " + COMMAND_WORD;
 
     public static final String SHOWING_HELP_MESSAGE = "Available commands:\n"
-            + "add n/NAME p/PHONE e/email a/address \n"
+            + "add n/NAME p/PHONE e/EMAIL a/ADDRESS [s/SUBJECT]\n"
             + "e.g., add n/James Ho p/22224444 e/jamesho@example.com"
-            + "a/123, Clementi Rd, 1234665 t/friend t/colleague \n"
+            + "a/123, Clementi Rd, 1234665 t/friend t/colleague s/math\n"
             + "delete INDEX e.g., delete 3 \n"
             + "list \n"
             + "clear \n"
             + "find KEYWORD [MORE KEYWORDS] e.g., find James Jake \n"
             + "help \n"
-            + "edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [t/TAG]..."
-            + "e.g.,edit 2 n/James Lee e/jameslee@example.com";
+            + "edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [t/TAG] [s/SUBJECT]...\n"
+            + "e.g., edit 2 n/James Lee e/jameslee@example.com \n"
+            + "relate a\\name1/name2/relationship1/relationship2 \n"
+            + "e.g., relate a\\James Lee Junior/James Lee/Son/Father\n"
+            + "relate d\\name1/name2/relationship1/relationship2 \n"
+            + "e.g., relate d\\James Lee Junior/James Lee/Son/Father";
 
     @Override
     public CommandResult execute(Model model) {
